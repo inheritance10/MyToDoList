988b134c3a4a35a7b9f879db214b2602f7425f44
inheritance10 <ali552001a@gmail.com>
2022-10-30 03:16:38 +0300

    to do list tamamlandi

--- App.js
+++ App.js
@@ -1,20 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+ // Import
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import {
+  Text,
+  SafeAreaView,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  Alert 
+} from 'react-native';
+
+import {appStyles as styles} from './styles';
+
+function App(){
+
+  const [task, setTask] = useState(""); //ARRAY DÖNDÜRÜR
+
+  const [taskArray, setTaskArray] = useState([]);
+
+  const AddTaskPress = () => {
+    setTaskArray([...taskArray, task]);
+    setTask("");
+  };
+
+  const deleteConfirm = (index) => {
+
+    Alert.alert(
+      'Confirm to submit',
+      'Are you sure to do this.',
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => DeleteTaskPress(index) }
+      ]
+    );
+  }
+
+  const DeleteTaskPress = (index) => {
+      const newTasks = [...taskArray];
+      newTasks.splice(index, 1);//hangi indexteki elemanı ve kaç tane sileceğini söyler
+      setTaskArray(newTasks);     
+  };
+
+  return(
+     <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+          <Text style={styles.title}>
+              My Tasks
+          </Text>
+          <Text style={styles.subtitle}>
+            Enter your tasks in the textbox and press Add button
+          </Text>
+
+          <TextInput 
+          value={task}
+          onChangeText={setTask}
+
+          style={styles.input} 
+          placeholder='Enter your task name'
+          
+          />
+
+          <TouchableOpacity 
+          onPress={AddTaskPress}
+          style={styles.buttonContainer}
+          >
+            <Text style={styles.buttonText}>
+              Add Task
+            </Text>
+
+          </TouchableOpacity>
+
+          <View style={styles.divider}/>
+
+          <FlatList 
+          data = {taskArray}
+          renderItem= {({item, index}) =>
+             <View style={styles.taskContainer}>
+                 <Text style= {styles.taskText}>{item}</Text>
+                 <TouchableOpacity 
+                     style={styles.taskDeleteButton}
+                     
+                     onPress= { () => deleteConfirm(index)}
+                    
+                    
+                    >
+                    <Text style={styles.taskDeleteText}>X</Text>
+                 </TouchableOpacity>
+             </View>
+          }
+          keyExtractor={(item) => item + Date.now() + Math.random()}
+          
+          />
+      </View>
+     </SafeAreaView>
+  )
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
